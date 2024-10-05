e7350ede8afe57ce4b4c26c9553b35b216ac2ec4
Solniechniy <oleksndrkorn@gmail.com>
2024-10-05 14:59:55 +0300

    fix

--- frontend/src/pages/Dashboard/components/Consumer/index.tsx
+++ frontend/src/pages/Dashboard/components/Consumer/index.tsx
@@ -1,12 +1,22 @@
-import { Program, Provider, web3 } from "@coral-xyz/anchor";
+import {
+  AnchorProvider,
+  BN,
+  Program,
+  Wallet,
+  utils,
+  web3,
+} from "@coral-xyz/anchor";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { SignerWalletAdapterProps } from "@solana/wallet-adapter-base";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { PublicKey, Signer, Transaction } from "@solana/web3.js";
-import BN from "bn.js";
+import {
+  useAnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+import { PublicKey, Transaction } from "@solana/web3.js";
 import { CalendarClock, LucideDollarSign, Users } from "lucide-react";
 import { useEffect } from "react";
-import { parse } from "uuid";
+import { parse, v4 } from "uuid";
 
 import { InfoCard } from "@/components/InfoCard";
 import { Calendar } from "@/components/ui/calendar";
@@ -15,7 +25,36 @@ import { IDL } from "@/providers/solana-provider/idl";
 
 import { Chart } from "./chart";
 
+export function padBuffer(buffer: Buffer | Uint8Array, targetSize: number) {
+  if (!(buffer instanceof Buffer)) {
+    buffer = Buffer.from(buffer);
+  }
+
+  if (buffer.byteLength > targetSize) {
+    throw new RangeError(`Buffer is larger than target size: ${targetSize}`);
+  }
+
+  return Buffer.concat(
+    [buffer, Buffer.alloc(targetSize - buffer.byteLength)],
+    targetSize,
+  );
+}
+
+export function bufferFromString(str: string, bufferSize?: number) {
+  const utf = utils.bytes.utf8.encode(str);
+
+  if (!bufferSize || utf.byteLength === bufferSize) {
+    return Buffer.from(utf);
+  }
+
+  if (bufferSize && utf.byteLength > bufferSize) {
+    throw RangeError("Buffer size too small to fit the string");
+  }
+
+  return padBuffer(utf, bufferSize);
+}
 const DEVNET_TOKEN_ACCOUNT = "4r1YezyMmxBTG9gukMPzubVUZUAV4JYTutNVzChHSQZv";
+const DEVNET_TOKEN_ADDRESS = "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr";
 const CONTRACT_ADDRESS = "2wivZHNNjvwWgrQEkGvv1bH9HgaWxXmfogkB24z1tsJz";
 
 export function findContractServiceAddress(
@@ -23,7 +62,7 @@ export function findContractServiceAddress(
   programId: PublicKey,
 ): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
-    [Buffer.from("service"), parse(id)],
+    [bufferFromString("service"), parse(id)],
     programId,
   );
 }
@@ -34,19 +73,30 @@ export async function createService(
   paymentDelegate: PublicKey,
   mint: PublicKey,
   sub_price: BN,
-  wallet: Signer,
-  provider: Provider,
+  sender: PublicKey,
   connection: web3.Connection,
-  signTransaction: SignerWalletAdapterProps["signTransaction"],
+  sendTransaction: SignerWalletAdapterProps["sendTransaction"],
+  wallet: Wallet,
 ) {
   const [service, bump] = findContractServiceAddress(
     id,
     new PublicKey(CONTRACT_ADDRESS),
   );
-  const sender = wallet.publicKey;
+  const provider = new AnchorProvider(connection, wallet, {
+    commitment: "confirmed",
+  });
+
   const program = new Program(IDL, new PublicKey(CONTRACT_ADDRESS), provider);
+  const allServices = await program.account.service.all();
+  console.log(allServices, "allServices");
   const createServiceInstruction = await program.methods
-    .createService(new BN(id), authority, paymentDelegate, sub_price, bump)
+    .createService(
+      new BN(parse(id), "be"),
+      authority,
+      paymentDelegate,
+      sub_price,
+      bump,
+    )
     .accounts({
       sender,
       service,
@@ -54,20 +104,27 @@ export async function createService(
       systemProgram: web3.SystemProgram.programId,
     })
     .instruction();
-
-  const blockHash = await connection.getLatestBlockhash();
   const transferTransaction = new Transaction().add(createServiceInstruction);
+  transferTransaction.feePayer = sender;
+  const {
+    context: { slot: minContextSlot },
+    value: { blockhash, lastValidBlockHeight },
+  } = await connection.getLatestBlockhashAndContext();
 
-  transferTransaction.feePayer = new PublicKey(CONTRACT_ADDRESS);
-  transferTransaction.recentBlockhash = blockHash.blockhash;
+  const simulate = await connection.simulateTransaction(transferTransaction);
+  console.log(simulate);
+  const signature = await sendTransaction(transferTransaction, connection, {
+    minContextSlot,
+  });
 
-  const signedTransaction = await signTransaction(transferTransaction);
+  const result = await connection.confirmTransaction({
+    blockhash,
+    lastValidBlockHeight,
+    signature,
+  });
+  console.log("result");
 
-  const signedTransactions = await connection.sendRawTransaction(
-    signedTransaction.serialize(),
-  );
-
-  return signedTransactions[0];
+  return result;
 }
 
 export const Consumer = () => {
@@ -75,9 +132,9 @@ export const Consumer = () => {
   const activeSubscriptions = mySubscriptions.filter(
     (el) => el.status === "processing",
   );
-  const { publicKey } = useWallet();
+  const { publicKey, sendTransaction } = useWallet();
   const { connection } = useConnection();
-
+  const wallet = useAnchorWallet();
   const getAccountAddress = async () => {
     if (publicKey) {
       const tokens = await connection.getParsedTokenAccountsByOwner(publicKey, {
@@ -91,7 +148,18 @@ export const Consumer = () => {
     }
   };
   const createSubscription = async () => {
-    console.log("creating");
+    if (publicKey && sendTransaction && wallet)
+      await createService(
+        v4(),
+        publicKey,
+        publicKey,
+        new PublicKey(DEVNET_TOKEN_ADDRESS),
+        new BN("333"),
+        publicKey,
+        connection,
+        sendTransaction,
+        wallet as Wallet,
+      );
   };
 
   useEffect(() => {
